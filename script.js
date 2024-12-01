b08ddba9074d99dce236fc4983a5de40776f65b6
Nahyaanp <nahyaanp@gmail.com>
2024-12-01 12:52:23 +0000

    Changes # 05

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-<<<<<<< HEAD
   $(window).scroll(function(){
       // sticky navbar on scroll script
       if(this.scrollY > 20){
@@ -78,83 +77,3 @@ $(document).ready(function(){
       }
   });
 });
-=======
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-  
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-  
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
-  
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-  
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Front end developer", "Designer", "Freelancer","Back End Web Developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    var typed = new Typed(".typing-3", {
-        strings: ["Connect with me on :)"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-  
-    var typed = new Typed(".typing-2", {
-        strings: ["Front end developer", "Designer", "Freelancer","Back End Web Developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-  
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
-  });
->>>>>>> 44c3c4427fc2bca61283c44ba7ec3fba1edad510
